58ec75e442792ffdc08f0ed8b371e9203efdb847
PhG22 <d3athr4it0@gmail.com>
2025-05-13 23:33:15 -0300

    Entrega 01 Concluída(?)

--- Teste_Cego01/explorer.py
+++ Teste_Cego01/explorer.py
@@ -8,10 +8,14 @@ import sys
 import os
 import random
 import math
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 from vs.abstract_agent import AbstAgent
 from vs.constants import VS
 from map import Map
+
+from numpy.random import choice
+from a_star import AStar
+import heapq
 
 class Stack:
     def __init__(self):
@@ -27,9 +31,27 @@ class Stack:
     def is_empty(self):
         return len(self.items) == 0
 
+class EdgeManager:
+    def __init__(self):
+        self.edges = {}
+
+    def add_edge(self, node1, node2, cost):
+        if node1 not in self.edges:
+            self.edges[node1] = {}
+        if node2 not in self.edges:
+            self.edges[node2] = {}
+        self.edges[node1][node2] = cost
+        self.edges[node2][node1] = cost
+
+    def check_edge(self, node1, node2):
+        return node1 in self.edges and node2 in self.edges[node1]
+
 class Explorer(AbstAgent):
-    def __init__(self, env, config_file, resc):
-        """ Construtor do agente random on-line
+    """ class attribute """
+    MAX_DIFFICULTY = 3             # the maximum degree of difficulty to enter into a cell
+    
+    def __init__(self, env, config_file, resc, priorities_vector):
+        """ Construtor do agente
         @param env: a reference to the environment 
         @param config_file: the absolute path to the explorer's config file
         @param resc: a reference to the rescuer agent to invoke when exploration finishes
@@ -37,7 +59,9 @@ class Explorer(AbstAgent):
 
         super().__init__(env, config_file)
         self.walk_stack = Stack()  # a stack to store the movements
+        self.walk_time = 0         # time consumed to walk when exploring (to decide when to come back)
         self.set_state(VS.ACTIVE)  # explorer is active since the begin
+        self.visited = set()
         self.resc = resc           # reference to the rescuer agent
         self.x = 0                 # current x position relative to the origin 0
         self.y = 0                 # current y position relative to the origin 0
@@ -47,72 +71,107 @@ class Explorer(AbstAgent):
 
         # put the current position - the base - in the map
         self.map.add((self.x, self.y), 1, VS.NO_VICTIM, self.check_walls_and_lim())
+        self.visited.add((self.x, self.y))
+        self.is_coming_back = False
+        self.back_plan = []        # the plan to come back to the base
+        self.back_plan_cost = 0    # the cost of the plan to come back to the base
+        self.a_start = AStar((0,0))
+        self.movements = priorities_vector
 
     def get_next_position(self):
-        """ Randomically, gets the next position that can be explored (no wall and inside the grid)
-            There must be at least one CLEAR position in the neighborhood, otherwise it loops forever.
-        """
-        # Check the neighborhood walls and grid limits
-        obstacles = self.check_walls_and_lim()
-    
-        # Loop until a CLEAR position is found
-        while True:
-            # Get a random direction
+            obstacles = self.check_walls_and_lim()
+            cost = self.map.get_difficulty((self.x, self.y))
+
+            for movement in self.movements:
+                dx, dy = Explorer.AC_INCR[movement]
+
+                if obstacles[movement] == VS.CLEAR and (self.x + dx, self.y + dy) not in self.visited:
+                    return (dx, dy)
+                
+                if obstacles[movement] == VS.CLEAR and (self.x + dx, self.y + dy) in self.visited and not self.a_start.check_edge((self.x, self.y), (self.x + dx, self.y + dy)):
+                    cost_neighbor = self.map.get_difficulty((self.x + dx, self.y + dy))
+
+                    if dx == 0 or dy == 0:
+                        cost = cost * self.COST_LINE
+                        cost_neighbor = cost_neighbor * self.COST_LINE
+
+                    else:
+                        cost = cost * self.COST_DIAG
+                        cost_neighbor = cost_neighbor * self.COST_DIAG
+
+                    self.a_start.add_edge((self.x, self.y), (self.x + dx, self.y + dy), cost, cost_neighbor)
+                    
             direction = random.randint(0, 7)
-            # Check if the corresponding position in walls_and_lim is CLEAR
-            if obstacles[direction] == VS.CLEAR:
-                return Explorer.AC_INCR[direction]
+            return Explorer.AC_INCR[direction]
         
     def explore(self):
-        # get an random increment for x and y       
-        dx, dy = self.get_next_position()
+            dx, dy = self.get_next_position()
 
-        # Moves the body to another position
-        rtime_bef = self.get_rtime()
-        result = self.walk(dx, dy)
-        rtime_aft = self.get_rtime()
-
-        # Test the result of the walk action
-        # Should never bump, but for safe functionning let's test
-        if result == VS.BUMPED:
-            # update the map with the wall
-            self.map.add((self.x + dx, self.y + dy), VS.OBST_WALL, VS.NO_VICTIM, self.check_walls_and_lim())
-            #print(f"{self.NAME}: Wall or grid limit reached at ({self.x + dx}, {self.y + dy})")
-
-        if result == VS.EXECUTED:
-            # check for victim returns -1 if there is no victim or the sequential
-            # the sequential number of a found victim
-            self.walk_stack.push((dx, dy))
-
-            # update the agent's position relative to the origin
-            self.x += dx
-            self.y += dy          
-
-            # Check for victims
-            seq = self.check_for_victim()
-            if seq != VS.NO_VICTIM:
-                vs = self.read_vital_signals()
-                self.victims[vs[0]] = ((self.x, self.y), vs)
-                print(f"{self.NAME} Victim found at ({self.x}, {self.y}), rtime: {self.get_rtime()}")
-                #print(f"{self.NAME} Seq: {seq} Vital signals: {vs}")
+            # checks whether the agent should backtrack due to all neighbors being visited
+            # if all neighbors are visited or bumps into a wall or limit, return to the previous position
+            if all([(self.x + incr[0], self.y + incr[1]) in self.visited 
+                    or self.check_walls_and_lim()[i] == VS.WALL 
+                    or self.check_walls_and_lim()[i] == VS.END for i, incr in Explorer.AC_INCR.items()]):
+                dx, dy = self.walk_stack.pop()
+                dx = -1 * dx
+                dy = -1 * dy
             
-            # Calculates the difficulty of the visited cell
-            difficulty = (rtime_bef - rtime_aft)
-            if dx == 0 or dy == 0:
-                difficulty = difficulty / self.COST_LINE
-            else:
-                difficulty = difficulty / self.COST_DIAG
+            # Moves the body to another position
+            rtime_bef = self.get_rtime()    # previous remaining time
+            result = self.walk(dx, dy)      # walk to the new position
+            rtime_aft = self.get_rtime()    # remaining time after the walk
 
-            # Update the map with the new cell
-            self.map.add((self.x, self.y), difficulty, seq, self.check_walls_and_lim())
-            #print(f"{self.NAME}:at ({self.x}, {self.y}), diffic: {difficulty:.2f} vict: {seq} rtime: {self.get_rtime()}")
 
-        return
+            # Should never bump, but for safe functionning let's test
+            if result == VS.BUMPED:
+                # update the map with the wall
+                self.map.add((self.x + dx, self.y + dy), VS.OBST_WALL, VS.NO_VICTIM, self.check_walls_and_lim())
+                
+            if result == VS.EXECUTED:
+                # store the new step (displacement) in the stack
+                if (self.x + dx,self.y + dy) not in self.visited:
+                    self.walk_stack.push((dx, dy))
+
+                self.visited.add((self.x + dx,self.y + dy))
+                # check for victim, returns -1 if there is no victim or the sequential
+                # the sequential number of a found victim
+
+                prev_x = self.x
+                prev_y = self.y
+                prev_diff = self.map.get_difficulty((prev_x, prev_y))
+
+                # update the agent's position relative to the origin
+                self.x += dx
+                self.y += dy
+
+                # update the walk time
+                self.walk_time = self.walk_time + (rtime_bef - rtime_aft)
+
+                # Check for victims
+                seq = self.check_for_victim()
+                if seq != VS.NO_VICTIM and seq not in self.victims:
+                    vs = self.read_vital_signals()
+                    # add the victim to the dictionary (vs[0] = victim id)
+                    self.victims[vs[0]] = ((self.x, self.y), vs)
+
+                # Calculates the difficulty (cost) of the visited cell
+                difficulty = (rtime_bef - rtime_aft)
+                if dx == 0 or dy == 0:
+                    prev_diff = prev_diff * self.COST_LINE
+                    self.a_start.add_edge((prev_x, prev_y), (self.x, self.y), difficulty, prev_diff)
+                    difficulty = difficulty / self.COST_LINE
+                else:
+                    prev_diff = prev_diff * self.COST_DIAG
+                    self.a_start.add_edge((prev_x, prev_y), (self.x, self.y), difficulty, prev_diff)
+                    difficulty = difficulty / self.COST_DIAG
+
+                # Update the map with the new cell
+                self.map.add((self.x, self.y), difficulty, seq, self.check_walls_and_lim())
+                
+            return
 
     def come_back(self):
         dx, dy = self.walk_stack.pop()
-        dx = dx * -1
-        dy = dy * -1
 
         result = self.walk(dx, dy)
         if result == VS.BUMPED:
@@ -123,26 +182,36 @@ class Explorer(AbstAgent):
             # update the agent's position relative to the origin
             self.x += dx
             self.y += dy
-            #print(f"{self.NAME}: coming back at ({self.x}, {self.y}), rtime: {self.get_rtime()}")
-        
+            
     def deliberate(self) -> bool:
-        """ The agent chooses the next action. The simulator calls this
-        method at each cycle. Must be implemented in every agent"""
+        time_tolerance = 2* self.COST_DIAG * Explorer.MAX_DIFFICULTY + self.COST_READ
 
-        consumed_time = self.TLIM - self.get_rtime()
-        if consumed_time < self.get_rtime():
+        # keeps exploring while there is enough time
+        if  self.back_plan_cost + time_tolerance < self.get_rtime():
             self.explore()
+
+            self.back_plan_cost = self.a_start.get_shortest_cost((self.x, self.y), (0,0))
+            
             return True
 
-        # time to come back to the base
+        if not self.is_coming_back:
+            self.is_coming_back = True
+            self.back_plan, self.back_plan_cost = self.a_start.calc_backtrack((self.x, self.y))
+            # updates walk_stack with the back_plan
+            self.walk_stack = Stack()
+            for action in self.back_plan[::-1]:
+                self.walk_stack.push(action)
+
+        # no more come back walk actions to execute or already at base
         if self.walk_stack.is_empty() or (self.x == 0 and self.y == 0):
-            # time to wake up the rescuer
-            # pass the walls and the victims (here, they're empty)
-            print(f"{self.NAME}: rtime {self.get_rtime()}, invoking the rescuer")
-            #input(f"{self.NAME}: type [ENTER] to proceed")
-            self.resc.go_save_victims(self.map, self.victims)
+            # time to pass the map and found victims to the master rescuer
+            self.resc.sync_explorers(self.map, self.victims)
+            # prints position and time of the explorer when finishes
+            print(f"{self.NAME}: at ({self.x}, {self.y}), rtime: {self.get_rtime()}")
+            # finishes the execution of this agent
             return False
 
+        # proceed to the base
         self.come_back()
         return True
 
--- Teste_Cego01/main.py
+++ Teste_Cego01/main.py
@@ -7,26 +7,46 @@ from vs.environment import Env
 from explorer import Explorer
 from rescuer import Rescuer
 
-def main(data_folder_name):
+def main(data_folder_name, config_ag_folder_name):
    
     # Set the path to config files and data files for the environment
     current_folder = os.path.abspath(os.getcwd())
+    config_ag_folder = os.path.abspath(os.path.join(current_folder, config_ag_folder_name))
     data_folder = os.path.abspath(os.path.join(current_folder, data_folder_name))
-
     
     # Instantiate the environment
     env = Env(data_folder)
     
-    # config files for the agents
-    rescuer_file = os.path.join(data_folder, "rescuer_config.txt")
-    explorer_file = os.path.join(data_folder, "explorer_config.txt")
-    
-    # Instantiate agents rescuer and explorer
-    resc = Rescuer(env, rescuer_file)
+    # Instantiate master_rescuer
+    # This agent unifies the maps and instantiate other 3 agents
+    rescuer_file = os.path.join(config_ag_folder, "rescuer_1_config.txt")
+    master_rescuer = Rescuer(env, rescuer_file, 4)   # 4 is the number of explorer agents
 
-    # Explorer needs to know rescuer to send the map
+    #Vetor de prioridade de movimentos
+    # v1 = [0, 1, 2, 3, 4, 5, 6, 7]  # Prioritize moving in a clockwise direction
+    # v2 = [0, 2, 4, 6, 1, 3, 5, 7]  # Prioritize moving in a zigzag pattern
+    # v3 = [7, 6, 5, 4, 3, 2, 1, 0]  # Prioritize moving in a counter-clockwise direction
+    # v4 = [7, 5, 3, 1, 6, 4, 2, 0]  # Prioritize moving in a reverse zigzag pattern
+
+    v1 = [2, 1, 0, 7, 6, 5, 4, 3]
+    v2 = [2, 3, 4, 5, 6, 7, 0, 1]
+    v3 = [6, 7, 0, 1, 2, 3, 4, 5]
+    v4 = [6, 5, 4, 3, 2, 1, 0, 7]
+
+    # Explorer needs to know rescuer to send the map 
     # that's why rescuer is instatiated before
-    exp = Explorer(env, explorer_file, resc)
+    for exp in range(1, 5):
+        filename = f"explorer_{exp:1d}_config.txt"
+        explorer_file = os.path.join(config_ag_folder, filename)
+        # Add vector of priorities to the explorer
+        if exp == 1:
+            Explorer(env, explorer_file, master_rescuer, v1)
+        elif exp == 2:
+            Explorer(env, explorer_file, master_rescuer, v2)
+        elif exp == 3:
+            Explorer(env, explorer_file, master_rescuer, v3)
+        else:
+            Explorer(env, explorer_file, master_rescuer, v4)
 
     # Run the environment simulator
     env.run()
@@ -39,6 +59,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         data_folder_name = sys.argv[1]
     else:
-        data_folder_name = os.path.join("datasets", "data_10v_12x12")
+        data_folder_name = os.path.join("datasets", "data_225v_100x80")
+        config_ag_folder_name = os.path.join("cfg_1")
         
-    main(data_folder_name)
+    main(data_folder_name, config_ag_folder_name)
